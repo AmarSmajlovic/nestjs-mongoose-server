74fe41b141be895fa8399bbe16b3acee300333a9
AmarSmajlovic <amarcsgo@gmail.com>
2023-05-25 23:11:47 +0200

    added create option restaurant

--- src/restaurant/restaurants.service.ts
+++ src/restaurant/restaurants.service.ts
@@ -2,10 +2,13 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Restaurant } from './restaurant.schema';
+import { CreateRestaurantDto } from './restaurant.dto';
 
 @Injectable()
 export class RestaurantsService {
-  constructor(@InjectModel(Restaurant.name) private restaurantModel: Model<Restaurant>) {}
+  constructor(
+    @InjectModel(Restaurant.name) private restaurantModel: Model<Restaurant>,
+  ) {}
 
   async findAll(): Promise<Restaurant[]> {
     return this.restaurantModel.find().exec();
@@ -16,4 +19,10 @@ export class RestaurantsService {
     return this.restaurantModel.find({ name: regex }).exec();
   }
 
+  async create(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
+    const createRestaurant = await this.restaurantModel.create(
+      createRestaurantDto,
+    );
+    return createRestaurant;
+  }
 }
