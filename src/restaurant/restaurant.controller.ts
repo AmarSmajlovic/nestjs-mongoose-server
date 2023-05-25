ce99dcbfd95e79825bd81d870be64d05ebf84cc7
Amar Smajlovic <80000802+AmarSmajlovic@users.noreply.github.com>
2023-05-25 23:12:34 +0200

    added create option restaurant (#1)

--- src/restaurant/restaurant.controller.ts
+++ src/restaurant/restaurant.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './restaurant.schema';
+import { CreateRestaurantDto } from './restaurant.dto';
 
 @Controller('restaurants')
 export class RestaurantController {
@@ -9,8 +10,13 @@ export class RestaurantController {
   @Get()
   async findAll(@Query('name') name?: string): Promise<Restaurant[]> {
     if (name) {
-        return this.restaurantService.searchByName(name);
-      }
-      return this.restaurantService.findAll();
+      return this.restaurantService.searchByName(name);
     }
+    return this.restaurantService.findAll();
+  }
+
+  @Post()
+  async create(@Body() createRestaurantDto: CreateRestaurantDto) {
+    await this.restaurantService.create(createRestaurantDto);
+  }
 }
